6665bf9bd62de56ac3bbeb1aad0d0b5983cd3ec1
kks940104 <kks940104@gmail.com>
2025-01-23 18:01:43 +0900

    .

--- src/main/java/org/anonymous/file/controllers/FileController.java
+++ src/main/java/org/anonymous/file/controllers/FileController.java
@@ -165,6 +165,16 @@ public class FileController {
         imageService.select(seq);
     }
 
+    /**
+     * 파일 그룹작업 완료 처리
+     * @param gid
+     * @param location
+     */
+    @GetMapping("/done/{gid}")
+    public void processDone(@PathVariable("gid") String gid, @RequestParam(name="location", required = false) String location) {
+        
+    }
+
     // region Test했던것
 
 
